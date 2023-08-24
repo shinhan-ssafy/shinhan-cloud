20f17d4bd72025dc003a5728f4f5a7aa087b16fd
ryuu <ryuu.public@gmail.com>
2023-08-24 09:17:55 +0900

    first commit

---
+++ src/main/java/com/nahshin/shinhancloud/ShinhanCloudApplication.java
@@ -0,0 +1,13 @@
+package com.nahshin.shinhancloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinhanCloudApplication.class, args);
+    }
+
+}
